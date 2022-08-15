29590430f646217f0cc0f6fe6f263e1754160fa5
vidyaumesh <110035647+vidyaumesh@users.noreply.github.com>
2022-08-15 22:46:50 +0530

    New code upload

---
+++ Inheritance.java
@@ -0,0 +1,21 @@
+package program;
+import java.util.Scanner;
+import java.util.ArrayList;
+public  class Inheritance {
+	
+
+
+	public static void main(String[] args) {
+		
+		     System.out.println("give a number");
+			Scanner s=new Scanner(System.in);
+			int no=s.nextInt();
+		do
+		{
+			System.out.println(no+"");
+			++no;
+		}
+		while(no<=10);
+	}
+}
+	
